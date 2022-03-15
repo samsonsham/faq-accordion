3d0a0c1ca4486bc6eaf069914adeceb7dbe3cc30
Samson Sham <samsonsham@gmail.com>
2022-03-15 12:48:53 +0000

    remove font size from css root and modify accordion functionality to expand one at a time

--- js/after.js
+++ js/after.js
@@ -2,14 +2,23 @@ const accListener = () => {
   const accs = document.querySelectorAll('.accordion-header');
   accs.forEach((acc) => {
     acc.addEventListener('click', (e) => {
-      e.currentTarget.classList.toggle('active');
       const panel = e.currentTarget.nextElementSibling;
       if (panel.style.maxHeight) {
+        e.currentTarget.classList.toggle('active');
         panel.style.maxHeight = null;
       } else {
+        collaspeAllPanels(accs);
+        e.currentTarget.classList.toggle('active');
         panel.style.maxHeight = panel.scrollHeight + 'px';
       }
     });
+  });
+};
+const collaspeAllPanels = (accs) => {
+  accs.forEach((acc) => {
+    const panel = acc.nextElementSibling;
+    panel.style.maxHeight = null;
+    acc.classList.remove('active');
   });
 };
 const init = () => {
